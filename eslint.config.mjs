1c9483a949ae45a5532e362782a14b45deb33364
Abdulaziz <alshehriofficial@gmail.com>
2026-09-16 08:26:23 +0300

    fix(governance): reconcile M028-M030 blocker packet with PR 58 correction

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,2 @@
 import tseslint from "typescript-eslint";
-export default [...tseslint.configs.recommended, { ignores: ["**/node_modules/**", "**/dist/**", "**/.next/**"] }];
+export default [...tseslint.configs.recommended, { ignores: ["**/node_modules/**", "**/dist/**", "**/.next/**", "**/next-env.d.ts"] }];
